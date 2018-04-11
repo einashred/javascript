f257c74bd7ecdd96e96628bf108a7e3b4c6b6536
stefaniemoessler <stefanie.moessler@gmx.net>
2018-04-11 17:48:32 +0200

    alles vor Benutzereingaben

---
+++ main.js
@@ -0,0 +1,60 @@
+//alert("Hello World");
+//ist ein Kommentar und dann nicht aktiv
+
+let nachricht = "Hallo Welt";
+//alert(nachricht);
+nachricht = "neuer Text";
+//alert(nachricht);
+console.log(nachricht);
+
+const linkColor = "#ff0000";
+
+let eine_variable = "test";
+console.log("Variable: eine_variable: ", eine_variable);
+
+let highScore = 75994.5;
+console.log("Highscore ist: ", highScore / 20);
+let meineNachricht = "test1";
+console.log(meineNachricht);
+meineNachricht = 'test2';
+console.log(meineNachricht);
+meineNachricht = `test3
+une ein 
+ganz langer
+text
+und aus`;
+console.log(meineNachricht)
+
+//Daten typ wahr oder falsch ist true falls
+let fullName = 'Jeffrey "The Dude" Lebowsky';
+console.log(fullName);
+
+let isOver18 = true;
+console.log("über 18?", isOver18);
+
+let age = 17;
+console.log("age über 18", age >18);
+
+let zahlenListe = [2, 11, 24, 33];
+console.log(zahlenListe);
+
+let namensListe =["Jane", "Marry"];
+console.log(namensListe[1]);
+console.log(namensListe.length);
+console.log(namensListe[2]);
+
+let benutzer = {
+    vorname: "Merry",
+    nachname: "Jane",
+    alter: 420
+};
+console.log("Benutzer Alter: ", benutzer.alter);
+benutzer.bestzeit = 200;
+console.log(benutzer);
+
+benutzer["bestzeit ever"] = 100;
+console.log(benutzer);
+
+
+
+
